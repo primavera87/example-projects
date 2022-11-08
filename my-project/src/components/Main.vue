313222f8ed1d1e02a71dd81538e6920287eaa4aa
primavera87 <primavera87@mail.ru>
2022-11-08 12:52:22 +0300

    main-menu

---
+++ my-project/src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main-menu">
+    <router-link to="Tables" class="main-menu__button">Таблицы</router-link>
+    <div class="main-menu__button">Графики</div>
+    <div class="main-menu__button">Чат</div>
+    <div class="main-menu__button">FAQ</div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Main"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ my-project/src/components/Tables.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="tables">
+    <router-link class="button-back" to="/">Назад</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tables"
+}
+</script>
+
+<style scoped>
+
+</style>
