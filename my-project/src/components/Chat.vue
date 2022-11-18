b136f1ddfce306819ba517b1ce48bc5f2841e769
primavera87 <primavera87@mail.ru>
2022-11-18 13:46:58 +0300

    chat-bot

--- my-project/src/components/Chat.vue
+++ my-project/src/components/Chat.vue
@@ -27,26 +27,206 @@
             </div>
           </div>
         </div>
+        <div class="chat-bot__message-box" v-if="showInputChat">
+          <first-variable-messages :chat-bot-answers="chatBotAnswers[0]"
+                           @actionCredit="actionCredit"
+                           v-if="showAnswers"
+          />
+          <div v-if="showInputChat">
+            <div class="chat-bot__input-block" v-if="this.botsMessages.length === 3">
+              <input v-model="messagesAnswers.nameMessage" class="chat-bot__input-message">
+              <button @click="sendMessage" class="chat-bot__button">Отправить</button>
+            </div>
+            <div class="chat-bot__input-block" v-if="this.botsMessages.length === 4">
+              <input v-model="messagesAnswers.mailMessage" class="lds-chat-bot__input-message"/>
+              <button @click="sendMessage" class="chat-bot__button">Отправить</button>
+            </div>
+            <div class="chat-bot__input-block" v-if="this.botsMessages.length === 5">
+              <input v-model="messagesAnswers.phoneMessage" class="chat-bot__input-message"/>
+              <button @click="sendMessage" class="chat-bot__button">Отправить</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import FirstVariableMessages from "./FirstVariableMessages.vue";
+import Vue from "vue";
 export default {
   name: "Chat",
+  components: {
+    FirstVariableMessages,
+  },
   data() {
     return {
       showInputChat: true,
+      chatBotAnswers: [],
+      showAnswers: true,
+      messagesAnswers: {
+        sumMessage: null,
+        creditHistory: null,
+        nameMessage: null,
+        mailMessage: null,
+        phoneMessage: null,
+      },
       botsMessages: [
         {
-          question: 'Привет! Я позитивная черепашка, помогаю справиться с психологическими проблемами при унизительном собеседовании. Чем могу помочь?',
+          question: 'Привет! Я позитивная черепашка, подбираю лучших психологов в нашем городе. Какие цены вас интересуют?',
           answer: undefined,
           loading: false,
         },
       ],
+      botsMessagesWithQuestions: [
+        {
+          question: 'Привет! Меня зовут Елена. Я бесплатно помогу Вам подобрать лучшее предложение среди микрозаймов. Одобренный займ вы сможете получить уже через несколько минут.\n' +
+            ' Какая сумма Вам нужна?',
+          answer: undefined,
+          loading: false,
+        }, {
+          question: 'Вам нужна женщина или мужчина',
+          answer: undefined,
+          loading: false,
+        }, {
+          question: 'Как Вас зовут?',
+          answer: undefined,
+          loading: false,
+        }, {
+          question: 'Укажите пожалуйста ваш E-mail',
+          answer: undefined,
+          loading: false,
+        }, {
+          question: 'Укажите пожалуйста Ваш номер телефона для смс подтверждений',
+          answer: undefined,
+          loading: false,
+        }
+      ],
+      answerForChatBot: [
+        {
+          answer: [
+            {
+              value: '2000',
+              name: 'до 2000',
+            },
+            {
+              value: '5000',
+              name: 'до 5000',
+            },
+            {
+              value: '10000',
+              name: 'до 10000',
+            }
+          ]
+        },
+        {
+          answer: [
+            {
+              value: 'women',
+              name: 'Женщина',
+            },
+            {
+              value: 'men',
+              name: 'Мужчина',
+            },
+            {
+              value: 'noMatter',
+              name: 'Все равно',
+            }
+          ]
+        }
+      ],
     }
   },
+  mounted() {
+    this.chatBotAnswers.push(this.answerForChatBot[0].answer);
+  },
+  methods: {
+    async actionCredit(value) {
+      if (value === 'до 2000' || value === '5000' || value === '10000') {
+        this.messagesAnswers.sumMessage = value;
+        this.chatBotAnswers = []
+        this.botsMessages[0] = {
+          question: 'Привет! Я позитивная черепашка психолог. Могу подобрать для вас лучших психологов нашего города. Какие цены вас интересуют',
+          answer: 'Нужна консультация ' + value + ' рублей',
+          loading: true,
+        };
+        setTimeout(() => {
+          this.botsMessages[0].loading = false;
+          this.botsMessages[1] = {
+            question: this.botsMessagesWithQuestions[1].question,
+            answer: undefined,
+            loading: false,
+          };
+          this.chatBotAnswers.push(this.answerForChatBot[1].answer);
+
+          this.scrollTick();
+        }, 2000);
+        this.scrollTick();
+      } else if (value === 'women' || value === 'man' || value === 'noMatter') {
+        this.messagesAnswers.creditHistory = value;
+        this.chatBotAnswers = []
+        this.botsMessages[1] = {
+          question: this.botsMessagesWithQuestions[1].question,
+          answer: value === 'women' ?
+            'Женщина' :
+            value === 'man' ?
+              'Мужчина' : 'Все равно',
+          loading: true,
+        };
+        setTimeout(() => {
+          this.showAnswers = false;
+          this.botsMessages[1].loading = false;
+          this.botsMessages[2] = {
+            question: this.botsMessagesWithQuestions[2].question,
+            answer: undefined,
+            loading: false,
+          };
+          this.scrollTick();
+        }, 2000);
+        this.scrollTick();
+      }
+    },
+
+    sendMessage(val) {
+      val.preventDefault();
+
+      if (this.botsMessages.length == 3) {
+        this.buildChat('nameMessage', 2, 3, 0);
+      }
+
+      if (this.botsMessages.length == 4) {
+        this.buildChat('mailMessage', 3, 4, 1);
+      }
+
+      if (this.botsMessages.length == 5) {
+        if (this.isEmpty(this.messagesAnswers.phoneMessage)) return;
+
+        Vue.set(this.botsMessages, 4, {
+          question: this.botsMessagesWithQuestions[4].question,
+          answer: this.messagesAnswers.phoneMessage,
+          loading: true,
+        });
+
+        setTimeout(() => {
+          Vue.set(this.botsMessages, 4, {
+            question: this.botsMessagesWithQuestions[4].question,
+            answer: this.messagesAnswers.phoneMessage,
+            loading: false,
+          });
+          this.showInputChat = false;
+
+        }, 2000);
+        this.scrollTick();
+      }
+    },
+    scrollTick() {
+      this.$nextTick(() => {
+        this.$refs.chatbox.scrollTop = this.$refs.chatbox.scrollHeight
+      })
+    },
+  }
 }
 </script>
 
---
+++ my-project/src/components/FirstVariableMessages.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="chat-bot__variable-block">
+    <div
+      v-for="modeItem in chatBotAnswers"
+      :key="modeItem.value"
+      v-model="answerOfChatBot"
+      class="chat-bot__radio-label"
+      @click="$emit('actionCredit', modeItem.value)"
+    >
+      {{ modeItem.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FirstVariableMessages",
+  data() {
+    return {
+      answerOfChatBot: '',
+    }
+  },
+  props: {
+    chatBotAnswers: {
+      type: Array,
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
